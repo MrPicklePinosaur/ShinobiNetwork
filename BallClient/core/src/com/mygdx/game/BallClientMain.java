1caf4906b97c924839944553563dd52d37199ad5
Shrey Mahey <shreymahey@gmail.com>
2019-04-23 12:38:48 -0400

    init

--- BallClient/core/src/com/mygdx/game/BallClientMain.java
+++ BallClient/core/src/com/mygdx/game/BallClientMain.java
@@ -2,75 +2,32 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
-import java.util.*;
-import java.io.*;
-import java.net.*;
-
 public class BallClientMain extends ApplicationAdapter {
-
-	//heavy lifters
 	SpriteBatch batch;
-	Camera camera;
-
-	BallClient server_socket;
-
-	Sprite background;
-
+	Texture img;
+	
 	@Override
 	public void create () {
-		//Init calls
-		Entity.init_textures("texture_lib.txt");
-		Gdx.graphics.setWindowedMode(Global.SCREEN_WIDTH,Global.SCREEN_HEIGHT);
-
-		//init variables
 		batch = new SpriteBatch();
-		camera = new Camera();
-
-		//init sprites (REMOVE LATER)
-		background = new Sprite(new Texture("background.png"));
-
-		//Init server
-		server_socket = new BallClient("127.0.0.1",5000);
-		server_socket.start_connection();
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
 		batch.begin();
-		background.draw(batch);
-		Entity.draw_all(batch);
+		batch.draw(img, 0, 0);
 		batch.end();
-
-		handleInput();
-
-		if (Entity.getClientEntity() != null) { camera.moveCam(Entity.getClientEntity()); }
-		batch.setProjectionMatrix(camera.getCam().combined);
-		camera.updateCam();
 	}
 	
 	@Override
 	public void dispose () {
-
-	}
-
-	public void handleInput() { //takes in user input and sends to server
-		String msg = "";
-		if (Gdx.input.isKeyPressed(Input.Keys.W)) { msg+=(",Key_W"); }
-		if (Gdx.input.isKeyPressed(Input.Keys.S)) { msg+=(",Key_S"); }
-		if (Gdx.input.isKeyPressed(Input.Keys.A)) { msg+=(",Key_A"); }
-		if (Gdx.input.isKeyPressed(Input.Keys.D)) { msg+=(",Key_D"); }
-		if (msg.equals("")) return; //if there isnt any input, don't send a message
-		msg = msg.substring(1); //get rid of extra comma in front
-
-		server_socket.send_msg(Global.MT_USIN,msg);
+		batch.dispose();
+		img.dispose();
 	}
 }
--- BallServer/core/src/com/mygdx/game/BallServerMain.java
+++ BallServer/core/src/com/mygdx/game/BallServerMain.java
@@ -5,58 +5,29 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.Vector2;
-import com.badlogic.gdx.physics.box2d.World;
-
-import javax.xml.crypto.Data;
-import java.io.IOException;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 public class BallServerMain extends ApplicationAdapter {
-
-	BallServer server;
+	SpriteBatch batch;
+	Texture img;
 	
 	@Override
 	public void create () {
-		//Misc inits
-		Entity.init_textures("texture_dimensions.txt");
-		Global.world = new World(new Vector2(0,0),true);
-
-		//Init server and such
-		server = new BallServer(5000);
-		server.start_server();
-
-		//Thread that listens for connecting users
-		new Thread(new Runnable() {
-
-			@Override
-			public void run() {
-				try {
-					//keep waiting for new clients to show up
-					while (true) {
-						BallClientHandler client = new BallClientHandler(server.getServerSocket().accept());
-						client.start_connection();
-					}
-				} catch(IOException ex) {
-					System.out.println(ex+" aka, something went wrong when connecting the player.");
-				}
-			}
-		}).start(); //auto start thread
+		batch = new SpriteBatch();
+		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-
-		//periodically send client position of all entities
-		String entity_data = Entity.send_all();
-		if (!entity_data.equals("")) { BallClientHandler.broadcast(Global.MT_UPDATE,Entity.send_all()); } //broadcast only if there is something to broadcast
-
+		batch.begin();
+		batch.draw(img, 0, 0);
+		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
-		server.close_server();
+		batch.dispose();
+		img.dispose();
 	}
 }

a93b3b1a3022343f94a240e215cfa3c67b1be43d
Daniel Liu <mr.picklepinosaur@gmail.com>
2019-04-23 12:24:40 -0400

    started working on maps

---
+++ BallServer/core/src/com/mygdx/game/Player.java
@@ -0,0 +1,13 @@
+package com.mygdx.game;
+
+public class Player extends Entity {
+
+    //TODO: LIST OF PROJECTILES THE PLAYER OWNS
+
+    public Player(String texture_path) {
+        super(texture_path);
+
+        //init player body
+    }
+
+}

87f8a5b5d4ce26857985221c6fae6f4c1828da42
Shrey Mahey <shreymahey@gmail.com>
2019-06-14 02:25:20 -0400

    Finished commenting HealthTracker

--- BallClient/core/src/com/mygdx/game/HealthTracker.java
+++ BallClient/core/src/com/mygdx/game/HealthTracker.java
@@ -15,6 +15,16 @@ min value being the minimum possible health (no health - 0), with
 the value of the ProgressBar knob being the frequency of the
 current health over the player's maximum health (this may vary
 depending on the player's class)
+
+Constructor:
+Methods (that aren't setters/getters):
+drawAll:
+    simply changes all HealthTrackers ProgressBars values to their respective players' health,
+    draws each HealthTracker ProgressBar relative to their respective players' position,
+    and draws each players' username tag at their respective positions
+update_data:
+    method that takes in data of all players of the players HealthTracker id with the player's health.
+    The HealthTracker associated with the players' id has its ProgressBar's value set to the player's health
 */
 
 import com.badlogic.gdx.Gdx;
@@ -30,36 +40,36 @@ import java.util.LinkedList;
 
 public class HealthTracker {
     private static HashMap<Integer,HealthTracker> health_bars = new HashMap<Integer,HealthTracker>();
-    public static final float y_offset = 25;
+    public static final float y_offset = 25;    //how much the healthbar is located above the player's body by
 
-    static Skin skin;
-    static ProgressBar.ProgressBarStyle style;
-    static Label.LabelStyle nameStyle;
+    static Skin skin;   //the Scene2d skin associated with each and every healthbar
+    static ProgressBar.ProgressBarStyle style;  //the style (background image, knob image of progressbar) of progressbar
+    static Label.LabelStyle nameStyle;  //the style of the username tag that is displayed above the player
 
-    private ProgressBar bar;
-    private Label nameLabel;
-    private int id;
+    private ProgressBar bar;    //the player's healthbar
+    private Label nameLabel;    //the player's username tag
+    private int id; //the player's id associated with each HealthTracker
 
     static{
-        skin = new Skin(Gdx.files.internal("gdx-skins/clean-crispy/skin/clean-crispy-ui.json"));
+        skin = new Skin(Gdx.files.internal("gdx-skins/clean-crispy/skin/clean-crispy-ui.json"));    //getting the skin
         style = new ProgressBar.ProgressBarStyle();
-        style.background = skin.getDrawable("progressbar-horizontal-c");
-        style.knobBefore = skin.getDrawable("progressbar-knob-horizontal-c");
-        style.background.setMinHeight(0f);
-        style.knobBefore.setMinHeight(style.knobBefore.getMinHeight()*0.3f);
+        style.background = skin.getDrawable("progressbar-horizontal-c");    //set progressbar's background image from skin json
+        style.knobBefore = skin.getDrawable("progressbar-knob-horizontal-c");   //set progressbar's knob image from skin json
+        style.background.setMinHeight(0f);  //set minimum height of the progressbar style in order to let us make the progressbar shorter than normally allowed
+        style.knobBefore.setMinHeight(style.knobBefore.getMinHeight()*0.3f);    //this is the minimum height that we actually set the healthbar's height to
         nameStyle = new Label.LabelStyle();
-        nameStyle.font = Global.skin.getFont("PixelFont");
-        nameStyle.fontColor = Color.BLACK;
+        nameStyle.font = Global.skin.getFont("PixelFont");  //set username tag's font from skin json
+        nameStyle.fontColor = Color.BLACK;  //set username tag's font color to black
     }
-    public HealthTracker(int id) {
-        this.id = id;
-        this.bar = new ProgressBar(0f,1f,0.005f,false,skin);
+    public HealthTracker(int id) {  //constructor that sets up the HealthTracker to keep track of a specific players' health
+        this.id = id;   //associate the player's id with the newly created HealthTracker
+        this.bar = new ProgressBar(0f,1f,0.005f,false,skin);    //our healthbar
         this.bar.setStyle(style);
         this.bar.setWidth(this.getBarWidth()*0.5f);
         this.bar.setHeight(this.getBar().getMinHeight());
         this.bar.setValue(1f);
 
-        health_bars.put(id,this);
+        health_bars.put(id,this);   //add new HealthTracker to list of HealthTrackers for all players
         Global.game.game_screen.getStage().addActor(bar);
 
         this.nameLabel = new Label(Global.user_data.username,skin);
@@ -68,6 +78,9 @@ public class HealthTracker {
     }
 
     public static void drawAll(SpriteBatch batch){
+        //go through all HealthTrackers,
+        //draw each healthbar (while updating it if necessary),
+        //and position + draw each username tag above each player
         for(HealthTracker ht : HealthTracker.health_bars.values()) {
 
             ht.getBar().act(Gdx.graphics.getDeltaTime());
@@ -78,6 +91,10 @@ public class HealthTracker {
     }
 
     public static void update_data(String[] data) {
+        //for all players, get the id that is associated
+        //with their HealthTrackers, and use it to make
+        //each player's unique HealthTracker update with
+        //the player's current health
         for (int i = 0; i < data.length; i ++) {
             String[] hp_data = data[i].split(",");
 
